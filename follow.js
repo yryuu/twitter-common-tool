3384e93ede36151d37676f00251b4720c0420181
yamazaki <yryuuyamazaki@gmail.com>
2018-07-22 17:12:55 +0900

    bug fix

--- follow.js
+++ follow.js
@@ -16,15 +16,14 @@ const client = new Twitter({
 
 getKeywordSearch();
 
-
 function getKeywordSearch() {
     const params = { q: keyword, include_entities: false }//200件まで取得可能。デフォルトは20
     client.get('search/tweets', params, function (error, tweets, response) {
         if (!error) {
             for (let i = 0; i < tweets.statuses.length; i++) {
-                console.log(tweets.statuses[i].id);
-                console.log(tweets.statuses[i].text);
-                follow(tweets.statuses[i].id);
+                // console.log(tweets.statuses[i].id_str);
+                console.log(tweets.statuses[i].user.id);
+                follow(tweets.statuses[i].user.id);
             }
         } else {
             console.log(error);
@@ -33,12 +32,8 @@ function getKeywordSearch() {
 }
 
 function follow(id) {
+    console.log("register" + id)
     const params = { user_id: id }
-    client.get('friendships/create', params, function (error, tweets, response) {
-        if (!error) {
-            console.log('登録成功したみたいだぜ');
-        } else {
-            console.log(error);
-        }
-    })
+    client.post('friendships/create', params);
+    console.log("registerEnd");
 }
